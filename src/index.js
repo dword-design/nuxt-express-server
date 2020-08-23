c493cf421fa642c95e9b2c910f094e92a5693f2a
dword <sebastianlandw@gmail.com>
2020-08-23 10:41:24 +0000

    fix: fixes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+import { mapValues, reduce, values } from '@dword-design/functions'
 import express from 'express'
-import { reduce, mapValues, values } from '@dword-design/functions'
 
-export default function (options) {
-  let { routes } = { ...this.options.expressServer, ...options }
-  if (!Array.isArray(routes)) {
-    routes = routes |> mapValues((handler, path) => ({ path, handler })) |> values
+export default function (moduleOptions) {
+  const options = { ...this.options.expressServer, ...moduleOptions }
+  if (!Array.isArray(options.routes)) {
+    options.routes =
+      options.routes
+      |> mapValues((handler, path) => ({ handler, path }))
+      |> values
   }
   this.options.serverMiddleware.push({
+    handler:
+      options.routes
+      |> reduce((api, route) => api.get(route.path, route.handler), express()),
     path: '/api',
-    handler: routes |> reduce((api, { path, handler }) => api.get(path, handler), express()),
   })
 }
